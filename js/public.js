c3fdf55b0dc9dc7b1acdce3e5e87b2f6c9109589
OA Wu <comdan66@gmail.com>
2016-06-05 23:01:48 +0800

    首頁完成

--- js/public.js
+++ js/public.js
@@ -3,6 +3,14 @@
  * @copyright   Copyright (c) 2016 OA Wu Design
  */
 
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.6&appId=640377126095413";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 $(function () {
 
 });
